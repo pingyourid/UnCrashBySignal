7377b822834517095d9d8d63c79ca509d5d967d7
zhangbin <418452686@qq.com>
2014-10-21 10:43:42 +0800

    update

--- MessageTransTest/AAA.m
+++ MessageTransTest/AAA.m
@@ -3,7 +3,7 @@
 //  MessageTransTest
 //
 //  Created by zhangbin on 14-10-20.
-//  Copyright (c) 2014年 yhd. All rights reserved.
+//  Copyright (c) 2014年 apple. All rights reserved.
 //
 
 #import "AAA.h"
